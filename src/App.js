742f36c8a65fa960d48a1e4f721b8a2854cb56f3
Victoria <tapia.victoria5070@gmail.com>
2019-01-09 15:20:30 -0800

    create nav bar and add basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TopNav from './Components/TopNav/TopNav';
 import * as api from './api';
 
 import './App.css';
@@ -19,18 +19,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <TopNav />
         </header>
       </div>
     );
---
+++ src/Components/TopNav/TopNav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './TopNav.css'
+
+const TopNav = () => {
+  return (
+    <nav className="topNav">
+      <h2>Photo Finder</h2>
+    </nav>
+  )
+};
+
+export default TopNav;
